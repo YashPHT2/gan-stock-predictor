20c277af477a6da64e5491b370e4428a9af970e4
YashPHT2 <yash2006mars@gmail.com>
2025-02-08 16:47:38 +0530

    fix: Handle edge case in data preprocessing

--- stock_predictor.py
+++ stock_predictor.py
@@ -268,3 +268,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-02-06T13:49:50
 # Simulated code change on 2025-02-06T21:08:28
 # Simulated code change on 2025-02-06T13:19:34
+# Simulated code change on 2025-02-08T16:47:38
