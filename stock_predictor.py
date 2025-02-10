e0427602f21cd9bb795a4e84b92eb52574442832
YashPHT2 <yash2006mars@gmail.com>
2025-02-10 23:47:31 +0530

    docs: Update README with new instructions

--- stock_predictor.py
+++ stock_predictor.py
@@ -293,3 +293,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-02-10T22:04:00
 # Simulated code change on 2025-02-10T10:46:58
 # Simulated code change on 2025-02-10T09:21:21
+# Simulated code change on 2025-02-10T23:47:31
