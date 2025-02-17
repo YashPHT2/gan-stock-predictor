77bf3c45e3ee71fa056e4198824954533954b1d5
YashPHT2 <yash2006mars@gmail.com>
2025-02-17 09:59:43 +0530

    feat: Add helper function for data parsing

--- stock_predictor.py
+++ stock_predictor.py
@@ -370,3 +370,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-02-17T22:10:09
 # Simulated code change on 2025-02-17T10:42:43
 # Simulated code change on 2025-02-17T12:55:32
+# Simulated code change on 2025-02-17T09:59:43
