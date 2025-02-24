3ed4a2965b32914152881a9c8fbb1fdf1bfcd82e
YashPHT2 <yash2006mars@gmail.com>
2025-02-24 17:24:59 +0530

    test: Add unit test for helper function

--- stock_predictor.py
+++ stock_predictor.py
@@ -443,3 +443,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-02-24T18:55:29
 # Simulated code change on 2025-02-24T23:51:27
 # Simulated code change on 2025-02-24T20:44:08
+# Simulated code change on 2025-02-24T17:24:59
