7dab58e5dc0174fe7abf5cea7df7425799d31a3a
YashPHT2 <yash2006mars@gmail.com>
2025-03-03 11:52:37 +0530

    style: Format code according to PEP8

--- stock_predictor.py
+++ stock_predictor.py
@@ -507,3 +507,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-03-03T14:49:53
 # Simulated code change on 2025-03-03T12:35:52
 # Simulated code change on 2025-03-03T21:35:52
+# Simulated code change on 2025-03-03T11:52:37
