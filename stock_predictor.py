ce3814b061b6462e42da1e0645f7a9f484697a5e
YashPHT2 <yash2006mars@gmail.com>
2025-03-13 18:30:18 +0530

    refactor: Improve variable naming for clarity

--- stock_predictor.py
+++ stock_predictor.py
@@ -611,3 +611,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-03-12T23:56:16
 # Simulated code change on 2025-03-13T13:12:19
 # Simulated code change on 2025-03-13T15:58:10
+# Simulated code change on 2025-03-13T18:30:18
