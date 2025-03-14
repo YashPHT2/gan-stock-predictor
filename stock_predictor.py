b2e40a33d852aaaa7b461fa64f8f790c77f36c0d
YashPHT2 <yash2006mars@gmail.com>
2025-03-14 13:40:19 +0530

    refactor: Simplify complex conditional logic

--- stock_predictor.py
+++ stock_predictor.py
@@ -620,3 +620,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-03-14T22:00:55
 # Simulated code change on 2025-03-14T16:49:17
 # Simulated code change on 2025-03-14T22:04:10
+# Simulated code change on 2025-03-14T13:40:19
