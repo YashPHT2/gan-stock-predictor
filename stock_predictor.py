0e6846ecfe61dcd5daf755b7ae31137408c91729
YashPHT2 <yash2006mars@gmail.com>
2025-03-24 16:08:55 +0530

    build: Update dependencies in requirements.txt

--- stock_predictor.py
+++ stock_predictor.py
@@ -735,3 +735,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-03-24T15:14:49
 # Simulated code change on 2025-03-24T12:32:09
 # Simulated code change on 2025-03-24T18:38:33
+# Simulated code change on 2025-03-24T16:08:55
