3210e114522f6e7b228664d06fec3a5350d38f0a
YashPHT2 <yash2006mars@gmail.com>
2025-03-25 11:49:03 +0530

    fix: Corrected typo in comments

--- stock_predictor.py
+++ stock_predictor.py
@@ -743,3 +743,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-03-24T09:35:52
 # Simulated code change on 2025-03-24T13:24:32
 # Simulated code change on 2025-03-25T20:32:47
+# Simulated code change on 2025-03-25T11:49:03
