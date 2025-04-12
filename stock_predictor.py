c0da09a627c3552dab6168707c225f772d3b4344
YashPHT2 <yash2006mars@gmail.com>
2025-04-12 23:36:06 +0530

    refactor: Minor code cleanup in data loading

--- stock_predictor.py
+++ stock_predictor.py
@@ -903,3 +903,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-04-12T16:34:50
 # Simulated code change on 2025-04-12T21:13:48
 # Simulated code change on 2025-04-12T17:27:37
+# Simulated code change on 2025-04-12T23:36:06
