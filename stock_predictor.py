3b27bb5bf157bcabf498eea60e7e8f60ff7e6305
YashPHT2 <yash2006mars@gmail.com>
2025-04-27 09:49:46 +0530

    docs: Add inline comments to model architecture

--- stock_predictor.py
+++ stock_predictor.py
@@ -1061,3 +1061,4 @@ if __name__ == '__main__':
 # Simulated code change on 2025-04-27T09:12:48
 # Simulated code change on 2025-04-27T16:25:07
 # Simulated code change on 2025-04-27T17:04:03
+# Simulated code change on 2025-04-27T09:49:46
